3f36702ee78ecef5f1135f9d991b2b22b0cb1327
qute012 <ejrwls012@gmail.com>
2021-02-08 10:44:31 +0000

    update transform

--- nosy/transform.py
+++ nosy/transform.py
@@ -1,3 +1,14 @@
 import numpy as np
+import os
 
-class Noise
+class NoiseSynthesis(object):
+    def __init__(self, sr=16000, method='static', backend='torch'):
+        self.sr = sr
+        self.method = method
+        self.backend = backend
+        self.noises = self._process_noise_dataset()
+        
+    def __call__(self, sig):
+        
+    def _process_noise_dataset(self, path='noise_dataset'):
+        
